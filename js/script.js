0110047b1b15a831cf99b27f5bfddaf74390b2c7
Nick Shillingford <nshillingford21@gmail.com>
2017-05-06 13:59:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    Typed.new('.typing', {
+        strings: ["welcome to my personal website.<br>thanks for checking it out.", 
+                  "scroll down to see more.",""],
+        typeSpeed: 16
+    });
+});
