fde1bc5584e3fa4201f2190da75db618007a215f
Nick Shillingford <nshillingford21@gmail.com>
2017-05-17 16:17:29 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+setTimeout(function() {
+    var textArea = document.createElement("textarea");
+    
+    textArea.textContent = "hey!";
+    
+    document.getElementById("one").appendChild(textArea);
+}, 1000);
