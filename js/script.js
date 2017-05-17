3830fc89463959b83845c281bc0f3f2f0a203846
Nick Shillingford <nshillingford21@gmail.com>
2017-05-17 15:40:00 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+setTimeout(function() {
+    var textArea = document.createElement("textarea");
+    
+    textArea.textContent = "hey!";
+    
+    document.getElementById("one").appendChild(textArea);
+}, 1000);
 
+/*
+$(window).scroll(function(event) {
+    scroll = $(window).scrollTop();
+    
+    if ((scroll > 300) && !(isVisible)) {
+        document.getElementById("bubble0").className += " bubble";
+        
+        setTimeout(function() { 
+            document.getElementById("bubble0").innerHTML = "javascript."; 
+        }, 1200);
+		
+		isVisible = true;
+    }
+});
+*/
