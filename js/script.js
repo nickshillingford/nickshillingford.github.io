6e672d3773cad017e782f5f64f7b7c7f919e6fe0
Nick Shillingford <nshillingford21@gmail.com>
2017-05-18 15:37:59 -0400

    Delete script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function() {
-    var textArea = document.createElement("textarea");
-    
-    textArea.textContent = "hey!";
-    
-    document.getElementById("one").appendChild(textArea);
-}, 1000);
