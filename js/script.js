77f36e8777b04d8c49516be5e94af19853f55827
Nick Shillingford <nshillingford21@gmail.com>
2018-02-20 15:53:28 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+  
+		Typed.new(".message", {
+			strings: ["hi.", "this website is under construction.", "it will be done soon."],
+			typeSpeed: 30
+		});
+  
+});
